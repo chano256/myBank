745f54722e7540ffdf81cc2e76e804afeab9f569
Allan Ochan <32319750+chano256@users.noreply.github.com>
2021-04-29 12:14:22 +0300

    Added validator

---
+++ CreditCardValidator.java
@@ -0,0 +1,47 @@
+package javaKing;
+
+import java.util.Scanner;
+
+public class CreditCardValidator {
+    public static void main(String[] args) {
+
+    }
+
+    /** Return true if the card number is valid */
+    public static boolean isValid(long number) {
+        return true;
+    }
+
+    /** Get the result from Step 2 */
+    public static int sumOfDoubleEvenPlace(long number) {
+        return 100;
+    }
+
+    /** Return this number if it is a single digit, otherwise, return * the sum of the two digits */
+    public static int getDigit(int number) {
+        return 1;
+    }
+
+    /** Return sum of odd place digits in number */
+    public static int sumOfOddPlace(long number) {
+        return 1 + 1;
+    }
+
+    /** Return true if the digit d is a prefix for number */
+    public static boolean prefixMatched(long number, int d) {
+        return true;
+    }
+
+    /** Return the number of digits in d */
+    public static int getSize(long d) {
+        return 200;
+    }
+
+    /** 
+     * Return the first k number of digits from number. 
+     * If the number of digits in number is less than k, return number.
+    */
+    public static long getPrefix(long number, int k) {
+        return 10;
+    }
+}
